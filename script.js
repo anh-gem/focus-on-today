269f1ae14e6c6f87d3925a3effb3b76d53981163
anh-gem <meghna.prakash.sarkar@gmail.com>
2024-06-29 11:27:24 +0530

    changes updated

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 const checkBoxList = document.querySelectorAll('.custom-checkbox')
 const inputFields = document.querySelectorAll('.goalText')
 const progress = document.getElementById("prgressValue")
+const msg = document.getElementById("message")
 
 var progressIncrement = 1;
 var i = 0;
 
-var allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+var allGoals = {
+    first:{
+        goal:'goal 1',
+        status: false
+    },
+    first:{
+        goal:'goal 2',
+        status: false
+    },
+    first:{
+        goal:'goal 3',
+        status: false
+    }
+}
+allGoals = JSON.parse(localStorage.getItem('allGoals')) || {}
+function messageText(i) {
+    if (i == 1) {
+        msg.innerText = 'Well begun is half done!';
+    }
+    else if (i == 2) {
+        msg.innerText = 'Just a step away, keep going!';
+    }
+    else if (i == 3) {
+        msg.innerText = 'Whoa! You just completed all the goals, time for chill :D';
+    }
+    else {
+        msg.innerText = 'Raise the bar by completing your goals!';
+    }
+    return msg.innerText;
+}
 
 checkBoxList.forEach((checkbox) => {
     checkbox.addEventListener('click', () => {
@@ -14,49 +44,54 @@ checkBoxList.forEach((checkbox) => {
         })
         if (filledAll) {
             checkbox.parentElement.classList.toggle('completed')
+            const input = checkbox.nextElementSibling;
             const inputId = checkbox.nextElementSibling.id;
             allGoals[inputId].status = !allGoals[inputId].status
-            console.log( allGoals[inputId].status)
-            if(allGoals[inputId].status == true){
-            i++;
-            progressIncrement = (i/3)*100;
-            progress.style.visibility ='visible';
-            progress.style.width =`${progressIncrement}%`;
-            progress.innerText = `${i}/3`;
+            console.log(allGoals[inputId].status)
+            if (allGoals[inputId].status == true) {
+                input.disabled = true
+                i++;
+                messageText(i);
+                progressIncrement = (i / 3) * 100;
+                progress.style.visibility = 'visible';
+                progress.style.width = `${progressIncrement}%`;
+                progress.innerText = `${i}/3`;
             }
-            else if(allGoals[inputId].status == false){
-            i--;
-            progressIncrement = (i/3)*100;
-            progress.style.width =`${progressIncrement}%`;
-            if(i==0){
-                progress.innerText = '';
+            else if (allGoals[inputId].status == false) {
+                input.disabled = false
+                i--;
+                messageText(i);
+                progressIncrement = (i / 3) * 100;
+                progress.style.width = `${progressIncrement}%`;
+                if (i == 0) {
+                    progress.innerText = '';
+                }
+                else {
+                    progress.innerText = `${i}/3`;
+                }
             }
-            else{
-            progress.innerText = `${i}/3`;
-            }
-            }
-            localStorage.setItem('allGoals',JSON.stringify(allGoals));
+            localStorage.setItem('allGoals', JSON.stringify(allGoals));
         }
-        else{
-            const warn = document.getElementById("warning") 
-            warn.style.display ="block"
+        else {
+            const warn = document.getElementById("warning")
+            warn.style.display = "block"
         }
     })
 })
 
-inputFields.forEach((input)=>{
-    input.value = allGoals[input.id].goal
+inputFields.forEach((input) => {
+    // input.value = allGoals[input.id].goal
 
-    input.addEventListener('click',()=>{
-        const warn = document.getElementById("warning") 
-            warn.style.display ="none"
-    } )
+    input.addEventListener('click', () => {
+        const warn = document.getElementById("warning")
+        warn.style.display = "none"
+    })
 
-    input.addEventListener('input',()=>{
-      allGoals[input.id] = {
-        goal:input.value,
-        status: false
-      }
-      localStorage.setItem('allGoals',JSON.stringify(allGoals));
+    input.addEventListener('input', () => {
+        allGoals[input.id] = {
+            goal: input.value,
+            status: false
+        }
+        localStorage.setItem('allGoals', JSON.stringify(allGoals));
     })
 })
